03d486264c25be78a4448d3a9b05d264874e7885
Raag2005 <oskar@raagart.dk>
2020-12-30 20:34:23 +0100

    Update script.js

--- html/script.js
+++ html/script.js
@@ -8,6 +8,17 @@ $(function() {
     }
 
     display(false)
+    
+    window.addEventListener('message', function(event) {
+        var item = event.data;
+        if (item.type === "ui") {
+            if (item.status == true) {
+                display(true)
+            } else {
+                display(false)
+            }
+        }
+    });
 
     document.onkeyup = function (data) {
         if (data.which == 27) {
